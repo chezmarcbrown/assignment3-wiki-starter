5f8c56b3c160707d6236dc50a96529ecafa53c2d
Hjriebe <hjriebe@alaska.edu>
2023-10-06 13:49:15 -0800

    Clickable entries

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("encyclopedia/css", views.entry, name="css")
+    path("wiki/<str:title>/", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -8,8 +9,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return render(request, "{% url 'css' %}", {
-        "entries":util.get_entry()
+def entry(request, title):
+    content = util.get_entry(title)
+    if content:
+        html_content = markdown(content)
+        return render(request, "{encyclopedia/entry.html", {
+            'title': title, 'content': html_content
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
     })
-
