75170cca86a2ce8a1c98742bfda77d295f87ce57
acslmarc <marc@chezbrown.org>
2023-10-11 13:11:01 -0800

    Just testing if I can create a PR that goes to Anthony

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # test 
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
     path("edit", views.edit, name="edit"),
