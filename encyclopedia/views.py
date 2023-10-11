44954c69a11cf5c71c0299e54ac0b6b87e8ecf60
Liam <helm_of_protection@hotmail.com>
2023-10-10 17:08:33 -0800

    Added markdown to html conversion function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
+
+def convert_to_html(page_title):
+    page = util.get_entry(page_title)
+    markdowner = markdown.Markdown()
+    if page == None:
+        return None
+    else:
+        return markdowner.convert(page)
 
 
 def index(request):
