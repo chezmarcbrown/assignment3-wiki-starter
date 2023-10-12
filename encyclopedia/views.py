8c52b6d82d629a2c7135b25554fba67c5b60e171
acslmarc <marc@chezbrown.org>
2023-10-12 15:07:44 -0800

    feedback; deets in upcming email

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,9 +18,10 @@ def entry(request, title):
 
 def search(request):
     q = request.GET.get('q').strip()
-    if q in util.list_entries():
+    if q in util.list_entries(): 
+        # note that the match succeeds only if the match is case-sensitive exact
         return redirect("entry", title=q)
-    return render(request, "encyclopedia/search.html", {"entries": util.search(q), "q": q})
+    return render(request, "encyclopedia/search.html", {"entries": util.search(q.lower()), "q": q})
 
 
 def random_page(request):
@@ -33,9 +34,11 @@ def create(request):
     if request.method == "POST":
         title = request.POST.get("title").strip()
         content = request.POST.get("content").strip()
-        if title == "" or content == "":
-            return render(request, "encyclopedia/add.html", {"message": "Can't save with empty field.", "title": title, "content": content})
-        if title in util.list_entries():
+        if title == "":
+            return render(request, "encyclopedia/add.html", {"message": "Can't create an entry with empty title.", "title": title, "content": content})
+        if content == "":
+            return render(request, "encyclopedia/add.html", {"message": "Can't create an entry with empty contents.", "title": title, "content": content})
+        if title.lower() in [e.lower() for e in util.list_entries()]:
             return render(request, "encyclopedia/add.html", {"message": "Title already exist. Try another.", "title": title, "content": content})
         util.save_entry(title, content)
         return redirect("entry", title=title)
