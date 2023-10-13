7ed2c92fbeafae9e85732bdfe6fd65a032a735e8
acslmarc <marc@chezbrown.org>
2023-10-12 18:40:55 -0800

    see email for deets

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
-    path("<str:title>", views.entry, name="entry"),
-    path("edit/", views.edit, name="edit"),
-    path("save/", views.save, name="save"),
+    # YIKES! This would cause problems when user creates an entry 
+    # named edit, save, edd, or rand
+    # path("<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    path("edit/<str:title>/", views.edit, name="edit"),
     path("add/", views.add, name="add"),
     path("rand/", views.rand, name="rand")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -23,68 +23,52 @@ def entry(request, title):
         })
     
 def search(request):
-    if request.method == 'POST':
-        search = request.POST['q'].strip()
-        title = f"Search Results: \"{search}\""
-        
-        entries = util.list_entries()
-        search_results = []
-        for entry in entries:
-            if search.lower() in entry.lower():
-                search_results.append(entry)
-        content = search_results
-
-        if search_results.count == 0:
-            content = f"No results found for {search}"
-
-        return render(request, "encyclopedia/search.html", {
-            "title": title,
-            "content": content
-        })
+    search = request.GET['q'].strip()
+    title = f"Search Results: \"{search}\""
     
-def edit(request):
-    if request.method == "POST":
-        title = request.POST['title']
-        content = util.get_entry(title)
+    entries = util.list_entries()
+    search_results = []
+    for entry in entries:
+        if search.lower() in entry.lower():
+            search_results.append(entry)
 
-        return render(request, "encyclopedia/edit.html", {
-            "title": title,
-            "content": content
-        })
+    # with a POST, you need to redirect to another page, not render a page. 
+    # you'll see the misbehavior when you hit the browser refresh
+    # after the search is performed. Alternatively, use a GET 
+    # method on form in search.html (as I've done) and render the page
+    return render(request, "encyclopedia/search.html", {
+        "title": title,
+        "entries": search_results
+    })
     
-def save(request):
+def edit(request, title):
     if request.method == "POST":
-        title = request.POST['title']
         content = request.POST['content']
         util.save_entry(title, content)
-        
-        content = util.convert_to_html(title)
-        return render(request, "encyclopedia/entry.html", {
+        return redirect("entry", title=title)
+    else:
+        return render(request, "encyclopedia/edit.html", {
             "title": title,
-            "content": content
+            "content": util.get_entry(title)
         })
+
 
 def add(request):
-    if request.method == "GET":
-        return render(request, "encyclopedia/add.html")
-    elif request.method == "POST":
+    # better to test for POST, and then default to the GET action
+    if request.method == "POST":
         title = request.POST['title']
         content = request.POST['content']
-        isExists = util.get_entry(title)
+        contents = util.get_entry(title)
 
-        if isExists is not None:
-            return render(request, "encyclopedia/error.html", {
-            "title": "Error",
-            "content": f"Page \"{title}\" already exists."
-        })
+        if contents is not None:
+            return render(request, "encyclopedia/add.html", {
+                "error_message": f"Page \"{title}\" already exists."
+            })
         else:
             util.save_entry(title, content)
-            content = util.convert_to_html(title)
-            return render(request, "encyclopedia/entry.html", {
-                "title": title,
-                "content": content
-            })
-        
+            return redirect('entry', title=title)
+    return render(request, "encyclopedia/add.html")
+   
 def rand(request):
     entries = util.list_entries()
     random_entry = random.choice(entries)
