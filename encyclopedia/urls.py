9ef2427a1cb8bbd4d1520025e98ef0a672f88304
Hjriebe <hjriebe@alaska.edu>
2023-10-16 16:02:04 -0800

    Done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("new_entry/", views.new_entry, name="new-entry"),
     path("random_entry/", views.random_entry, name="random-entry"),
+    path("new_entry_form/", views.new_entry_form, name="new-entry-form"),
     path("wiki/<str:title>/edit", views.edit_entry, name="edit-entry")
 ]
